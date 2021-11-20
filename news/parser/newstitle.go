1996f271a8adcdd04810ef62b9ed473773d69c51
Aioloscn <756243011@qq.com>
2021-11-21 01:12:17 +0800

    Update url matching rules

--- news/parser/newstitle.go
+++ news/parser/newstitle.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-var newsTitleRe = regexp.MustCompile(`<a href="//(www.chinanews.com/.+.shtml)">([^<]+)</a></div>`)
+var newsTitleRe = regexp.MustCompile(`<a href="(http://www.chinanews.com.cn/[a-z]+/[0-9]+/[0-9]+-[0-9]+/.+.shtml)">([^<]+)</a></div>`)
 
 func ParseNewsTitle(contents []byte) engine.ParseResult {
 	matches := newsTitleRe.FindAllSubmatch(contents, -1)
@@ -13,7 +13,7 @@ func ParseNewsTitle(contents []byte) engine.ParseResult {
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		result.Requests = append(result.Requests, engine.Request{
-			Url: "https://" + string(m[1]),
+			Url: string(m[1]),
 			ParserFunc: func(c []byte) engine.ParseResult {
 				return ParseProfile(c)
 			},
